97fb1a8e46a7cbcd61d0f6bde8d03f06fb1219f0
ismazhiluk <ismazhiluk@gmail.com>
2016-10-18 22:38:24 +0500

    HW-2

--- Homework/HW-2/Properties/AssemblyInfo.cs
+++ Homework/HW-2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Homework")]
+[assembly: AssemblyTitle("HW-2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Homework")]
+[assembly: AssemblyProduct("HW-2")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("23d7068d-0619-44ca-8e91-f990d0907238")]
+[assembly: Guid("2d964fa0-2f0c-479b-9160-b1d6a2ec5fac")]
 
 // Version information for an assembly consists of the following four values:
 //
